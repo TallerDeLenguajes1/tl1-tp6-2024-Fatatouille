69c994101bf3e82a6fbf7c4519ae8834a18860ec
Gimenez Francisco Miguel <franciscomguelgimenez708@gmail.com>
2024-07-23 01:56:18 -0300

    punto 1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*Console.WriteLine("Hello, World!");
 
 int a;
 int b;
@@ -7,4 +7,25 @@ a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b);
+*/
+
+/* EJERCICIO 1
+
+int invertir, aux, res=0;
+Console.WriteLine("Ingrese un número: ");
+if (int.TryParse(Console.ReadLine(), out invertir)){
+    if(invertir>0){
+        aux=invertir;
+        while(aux>0){
+            res=res*10+(aux%10);
+            aux = aux/10;
+        }
+        Console.WriteLine($"El número que quiere invertir es {invertir}");
+        Console.WriteLine($"El número invertido es {res}");
+    }else{
+        Console.WriteLine("El numero ingresado es negativo");
+    }
+}else{
+    Console.WriteLine("No se ingreso un numero");
+}*/
 
