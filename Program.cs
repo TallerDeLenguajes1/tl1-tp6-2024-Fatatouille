0461cb9349e5f7dce6e5c400a67577d9d9216ca8
Gimenez Francisco Miguel <franciscomguelgimenez708@gmail.com>
2024-07-23 21:47:59 -0300

    Combinacion

--- Program.cs
+++ Program.cs
@@ -10,8 +10,6 @@ Console.WriteLine("valor de b:"+b);
 
 
 */
-Console.WriteLine("Ingrese un número");
-float num1= (float) Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("eliga una opcion:");
 Console.WriteLine("1: valor absoluto");
 Console.WriteLine("2: cuadrado");
@@ -21,95 +19,86 @@ Console.WriteLine("5: coseno");
 Console.WriteLine("6: parte entera");
 Console.WriteLine("7: maximo de dos numeros");
 Console.WriteLine("8: minimo de dos numeros");
+Console.WriteLine("9: Suma");
+Console.WriteLine("10: resta");
+Console.WriteLine("11: multiplicacion");
+Console.WriteLine("12: divison");
+
 int opcion = Convert.ToInt32(Console.ReadLine());
 
 float resultado;
 int calcula=1;
 while(calcula==1){
-if (num1>0){
-    switch(opcion){
-    case 1:
-        resultado=Math.Abs(num);
-        Console.WriteLine(resultado);
-        break;
-    case 2:
-        resultado=(float)Math.Pow(num, 2);
-        Console.WriteLine(resultado);
-        break;
-    case 3:
-        resultado=(float)Math.Sqrt(num);
-        Console.WriteLine(resultado);
-        break;
-    case 4:
-        resultado=(float)Math.Sin(num);
-        Console.WriteLine(resultado);
-        break;
-    case 5:
-        resultado=(float)Math.Cos(num);
-        Console.WriteLine(resultado);
-        break;
-    case 6:
-        int entera=(int)num;
-        Console.WriteLine(entera);
-        break;
-    case 7:
-        float numero2 = (float) Convert.ToDouble(Console.ReadLine());
-        if(num>numero2){
-            Console.WriteLine(num);
-        }else{
-            Console.WriteLine(numero2);
+    Console.WriteLine("Ingrese un número");
+    float num1= (float) Convert.ToDouble(Console.ReadLine());
+    if (num1>0){
+        float Num2 = 0;
+        if(opcion>=7){
+            Console.WriteLine("Escriba otro numero");
+            Num2= (float) Convert.ToDouble(Console.ReadLine());
         }
-        break;
-    case 8:
-        float numero = (float) Convert.ToDouble(Console.ReadLine());
-        if(num<numero){
-            Console.WriteLine(num);
-        }else{
-            Console.WriteLine(numero);
+        switch (opcion){
+            case 1:
+                resultado=Math.Abs(num1);
+                Console.WriteLine(resultado);
+                break;
+            case 2:
+                resultado=(float)Math.Pow(num1, 2);
+                Console.WriteLine(resultado);
+                break;
+            case 3:
+                resultado=(float)Math.Sqrt(num1);
+                Console.WriteLine(resultado);
+                break;
+            case 4:
+                resultado=(float)Math.Sin(num1);
+                Console.WriteLine(resultado);
+                break;
+            case 5:
+                resultado=(float)Math.Cos(num1);
+                Console.WriteLine(resultado);
+                break;
+            case 6:
+                int entera=(int)num1;
+                Console.WriteLine(entera);
+                break;
+            case 7:
+                if(num1>Num2){
+                    Console.WriteLine(num1);
+                }else{
+                    Console.WriteLine(Num2);
+                }
+                break;
+            case 8:
+                if(num1<Num2){
+                    Console.WriteLine(num1);
+                }else{
+                    Console.WriteLine(Num2);
+                }
+                break;
+            case 9:
+                resultado=num1+Num2;
+                Console.WriteLine("El resultado es: "+ resultado);
+                break;
+            case 10:
+                resultado=num1-Num2;
+                Console.WriteLine("El resultado es: "+ resultado);
+                break;
+            case 11:
+                resultado=num1*Num2;
+                Console.WriteLine("El resultado es: "+ resultado);
+                break;
+            case 12:
+                resultado=num1/Num2;
+                Console.WriteLine("El resultado es: "+ resultado);
+                break;
+            default:
+                Console.WriteLine("No se eligio una opcion valida");
+                break;
         }
-        break;
-    default:
-        Console.WriteLine("No se eligio una opcion valida");
-        break;
+    }else{
+        Console.WriteLine("No se puede operar con un numero negativo");
     }
-}else{
-    Console.WriteLine("No se puede operar con un numero negativo");
-}
-
-int res;
-
-
-    Console.WriteLine("Seleccione una operacion");
-    Console.WriteLine("1: suma, 2: resta, 3:multiplicar, 4:dividir");
-    int operacion = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Ingrese un numero");
-    int num1= Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Ingrese otro numero");
-    int num2= Convert.ToInt32(Console.ReadLine());
-
-    switch (operacion)
-    {
-        case 1:
-            res=num1+num2;
-            Console.WriteLine("El resultado es: "+ res);
-            break;
-        case 2:
-            res=num1-num2;
-            Console.WriteLine("El resultado es: "+ res);
-            break;
-        case 3:
-            res=num1*num2;
-            Console.WriteLine("El resultado es: "+ res);
-            break;
-        case 4:
-            res=num1/num2;
-            Console.WriteLine("El resultado es: "+ res);
-            break;
-        default:
-            Console.WriteLine("No se ingreso operacion valida");
-            break;
-    }
-    Console.WriteLine("Quiere realziar otro calculo? (1: si, 2: no)");
-    calcula=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Quiere realziar otro calculo? (1: si, 2: no)");
+        calcula=Convert.ToInt32(Console.ReadLine());
 }
